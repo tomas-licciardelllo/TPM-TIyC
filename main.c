ff205c2a4a2c24f410242fc0f1279654edc9fd07
tomi-licciardelllo <tomilicciardello@gmail.com>
2026-04-14 02:17:21 -0300

    Casi terminado Hamming

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void imprimirB(char c)
+#include <math.h>
+int imprimirB(unsigned char c)
 {
     int i;
     for(i = 7; i >= 0;  i --)
@@ -11,8 +11,45 @@ void imprimirB(char c)
 }
 
 
+int* Hamming(int cadena[], int modulo, int limite)
+{
+    int i,suma;
+    int n;
+    int m = 0;
+    while(limite!= m)
+    {
+        printf("Entro");
+        for(i=1; i<=modulo; i ++)
+        {
+            if((i & (i-1)) == 0)
+            {
+                for(n=i; n<=modulo; n++)
+                {
+                    if((n & (n-1) != 0) && ((n & i)>>(int)log2(i)))
+                   {
+                        suma += cadena[n];
+                        m++;
+                   }
+                }
+                m++;
+                if(suma % 2 == 0)
+                {
+                     cadena[i] = suma;
+                }
+                else
+                {
+                    cadena[i] = suma + 1;
+                }
+            }
+        }
 
-int Hamming(int *cadena, int modulo)
+    }
+    return cadena;
+}
+
+
+
+/*int Hamming(int *cadena, int modulo)
 {
     int cantidad;
     switch (modulo)
@@ -52,11 +89,16 @@ int Hamming(int *cadena, int modulo)
         p--;
     }
     return 0;
-}
+}*/
 
 
 int main()
 {
+    int *chain = malloc(2 *sizeof(int));
+    chain[0] = 0;
+    int i,n,t;
+    i = 1;
+    n = 1;
     FILE *arch;
     arch = fopen("ej.txt","rb");
     if(arch == 'NULL')
@@ -65,28 +107,39 @@ int main()
         return 0;
     }
 
-    fseek(arch, 0, SEEK_END);
-    long largo = ftell(arch);
-    fseek(arch,0, SEEK_SET);
-    char *buffer = malloc(largo);
-    if(buffer)
-    {
-        fread(buffer, 1,largo,arch);
-    }
-
-    printf("%s\n", buffer);
-    /*int caracter;
+    int caracter;
     while((caracter = fgetc(arch))!= EOF)
     {
-        printf("%d", caracter);
-        imprimirB((char)caracter);
-    }*/
-    fclose(arch);
+        printf("%c", caracter);
+        printf("no salgp\n");
+        imprimirB((unsigned char)caracter);
+        for(t= 7; t>=0; t--)
+        {
+            n+=1;
+            chain = realloc(chain, n * sizeof(int));
+            chain[i]= (((unsigned char)caracter >> t) & 1);
+            i++;
+        }
 
+    }
+    fclose(arch);
 
     int salir = 0;
 
+    i =0;
+    printf("MOSTRAME ESTO %d\n",n);
+    while(i<n)
+    {
+      printf("%d", chain[i]);
+      i++;
+    }
+    printf("\n");
+    //Hamming(chain,8,n);
     while(salir == 0){
+
+
+        printf("%d \n",(int)log2(32));
+
         printf("Ingrese una opcion\n");
         printf("[1] Leer archivo txt\n");
             salir =1;
