8c3fdbdf636f619526951a1e8742aa8893ab124d
santiagoguinazu7 <santiagoguinazu10@gmail.com>
2026-04-20 01:32:07 -0300

    Laboratorio 1: Ahora si, codigo completo con Hamming y errores

--- main.c
+++ main.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int imprimirB(unsigned char c)
-{
-    int i;
-    for(i = 7; i >= 0;  i --)
-    {
-        printf("%d", (c >> i) & 1);
+#include <time.h>
 
+
+void imprimirB(unsigned char c);
+int* Hamming(int cadena[], int modulo, int limite, int *arr_out);
+void introducir_errores(int *cadena_h, int modulo, int largo_total);
+int* decodificarHamming(int cadena_H[], int modulo, int largo_total, int *largo_info);
+void generarArchivoDEX(int *cadena_H, int modulo, int largo_total, char *nombre_salida);
+
+
+
+void imprimirB(unsigned char c) {
+    int i;
+    for (i = 7; i >= 0; i--) {
+        printf("%d", (c >> i) & 1);
     }
-printf("\n");
+    printf("\n");
 }
 
+int* Hamming(int cadena[], int modulo, int limite, int *arr_out) {
+    int *cadena_h = malloc(1 * sizeof(int));
+    int n = 1, indice = 1, arr = 1, i = 1, contador = 1;
+    int suma = 0, ind_H = 1, x, aux;
+
+    cadena_h[0] = 0;
 
 
-
-int* Hamming(int cadena[], int modulo, int limite)
-{
-    int *cadena_h = malloc(1*sizeof(int));
-    cadena_h[0]=0;
-    int n=1;
-    int indice = 1;
-    int arr = 1;
-    int i = 1;
-    int contador = 1;
-    int suma=0;
-    int suma_total = 0;
-    while(contador<limite)
-    {
-        while(indice<=modulo)
-        {
-            if((indice & (indice - 1))== 0)
-            {
-                arr++;
-                cadena_h = realloc(cadena_h, arr * sizeof(int));
-                cadena_h[n] = 2;
-            }
-            else
-            {
-                arr++;
-                cadena_h = realloc(cadena_h, arr * sizeof(int));
+    while (contador < limite) {
+        while (indice <= modulo && contador < limite) {
+            arr++;
+            cadena_h = realloc(cadena_h, arr * sizeof(int));
+            if ((indice & (indice - 1)) == 0) {
+                cadena_h[n] = 2; // Placeholder
+            } else {
                 cadena_h[n] = cadena[i];
                 i++;
                 contador++;
@@ -47,263 +42,143 @@ int* Hamming(int cadena[], int modulo, int limite)
             indice++;
             n++;
         }
-        indice=1;
-    }
-    int x = 1;
-    int aux;
-    contador = 1;
-    int ind_H=1;
-    int ind_p = 1;
-    while(contador < arr)
-    {
-        while(indice < modulo)
-        {
-            if((indice & (indice - 1))== 0)
-            {
-                n = indice;
-                aux  = ind_H;
-                while(n<modulo)
-                {
-                    if(((indice & n) == indice) && ((n & (n-1))!= 0))
-                    {
-                        suma += cadena_h[ind_H];
-                    }
-                    ind_H++;
-                    n++;
-                }
-                ind_H = aux;
-                if(suma % 2 == 0)
-                {
-                    cadena_h[ind_H] = 0;
-                }
-                else
-                {
-                    cadena_h[ind_H] = 1;
-                }
-                ind_p = ind_p * 2;
-                suma = 0;
-            }
-            indice++;
-            contador++;
-            ind_H++;
-        }
-        ind_p = 1;
-        x = (ind_H - modulo) + 1;
-        while(x<ind_H)
-        {
-            suma_total+=cadena_h[x];
-            x++;
-        }
-         if(suma_total % 2 == 0)
-        {
-            cadena_h[ind_H] = 0;
-        }
-        else
-        {
-            cadena_h[ind_H] = 1;
-        }
-        suma_total=0;
         indice = 1;
-        ind_H++;
+    }
+
+    contador = 1;
+    ind_H = 1;
+    while (contador < arr) {
+        if ((contador & (contador - 1)) == 0) {
+            suma = 0;
+
+            for (x = 1; x < arr; x++) {
+                if ((x & contador) && (x != contador)) {
+                    suma ^= cadena_h[x];
+                }
+            }
+            cadena_h[contador] = suma;
+        }
         contador++;
     }
+
+    *arr_out = arr;
     return cadena_h;
 }
 
-/*int* Hamming(int cadena[], int modulo, int limite)
-{
-    int *cadena_H = malloc(1 *sizeof(int));
-    int arr = 1;
-    cadena_H[0] = 0;
-    int i,suma=0;
-    int n=1;
-    int m = 1;
-    int indice = 1;
-    int cantidad=0;
-    switch(modulo)
-    {
-        case 8:
-            cantidad = 4;
-        break;
-        case 1024:
-            cantidad = 1013;
-        break;
-        case 16384:
-            cantidad = 16369;
-        break;
-    }
-    int check = 0;
-    int p=0;
-    while(limite!= m)
-    {
-        printf("Entro\n");
-        for(i=1; i<=modulo; i ++)
+void introducir_errores(int *cadena_h, int modulo, int largo_total) {
+    int i, posicion_relativa, posicion_absoluta;
+    srand(time(NULL)); //semilla
+    for (i = 1; i < largo_total; i += modulo)
         {
-            if((i & (i-1)) == 0)
-            {
-                p=i;
-                /*for(n=i; n<=cantidad; n++)
-                {
-                    check = (check << (int)log2(i)) | 1;
-                    if((n & (n-1) != 0) && (p & i))
-                   {
-                        suma += cadena[n];
-                   }
-                   p++;
-                }
-                n=indice;
-                while(n<=cantidad)
-                {
-                    if((p&i) && ((p & (p-1)) != 0))
-                    {
-                        suma +=cadena[n];
-                        n++;
-                    }
-                    else
-                    {
-                        if((p&i)==0 && ((p & (p-1)) != 0))
-                        {
-                            n++;
-                        }
-                    }
-                    p++;
-                }
-                if(suma % 2 == 0)
-                {
-                     indice++;
-                     cadena_H = realloc(cadena_H,arr *sizeof(int));
-                     cadena_H[i] = suma;
-
-                }
-                else
-                {
-                    indice++;
-                     cadena_H = realloc(cadena_H,arr *sizeof(int));
-                     cadena_H[i] = suma + 1;
-                }
-                suma = 0;
-            }
-            else
-            {
-                indice++;
-                cadena_H = realloc(cadena_H,arr *sizeof(int));
-                cadena_H[i] = cadena[i];
-                m++;
+        if (rand() % 2 == 0) {
+            posicion_relativa = rand() % modulo;
+            posicion_absoluta = i + posicion_relativa; //como es un arreglo que tiene todos los bloques juntos, sumamos el inicio del bloque con la posicion elegida para saber que pos hay q cambiar
+            if (posicion_absoluta < largo_total) { // este if evita acceder a una parte de la memoria q no exista , por las dudash
+                printf("\n--- Error introducido en posicion: %d ---", posicion_absoluta);
+                cadena_h[posicion_absoluta] ^= 1;
             }
         }
     }
-    return cadena_H;
-}*/
+}
 
+int* decodificarHamming(int cadena_H[], int modulo, int largo_total, int *largo_info) {
+    int p = 0, posicion_error = 0, i, j, pos_p, suma_xor, idx_info;
+    int *informacion;
 
+    while ((1 << p) < (modulo + 1)) p++; // la paridad breooo
 
-/*int Hamming(int *cadena, int modulo)
-{
-    int cantidad;
-    switch (modulo)
-    {
-        case 8:
-            cantidad = 4;
-            p = 4;
-        break;
-        case 1024:
-            info = 1013;
-            p = 10;
-        break;
-        case 16384:
-            cantidad = 16369;
-            p = 14;
-        break;
+    // Calculo del Síndrome , revisamos los bits de control
+    for (i = 0; i < p; i++) {
+        pos_p = (1 << i);
+        suma_xor = 0;
+        for (j = 1; j < largo_total; j++) {
+            if (j & pos_p) suma_xor ^= cadena_H[j]; // la suma xor, si hay numero impar de 1, nos da != 0
+        }
+        if (suma_xor != 0) posicion_error += pos_p; // nos da la posicion de error
     }
 
+    // Correccionn de error
+    if (posicion_error > 0 && posicion_error < largo_total) {
+        printf("\n¡Error detectado y corregido en la posicion: %d!", posicion_error);
+        cadena_H[posicion_error] ^= 1;
+    }
+    //extraemos la info
+    informacion = malloc(largo_total * sizeof(int));
+    idx_info = 0;
+    for (i = 1; i < largo_total; i++) {
+        if ((i & (i - 1)) != 0) {
+            informacion[idx_info++] = cadena_H[i];
+        }
+    }
+    *largo_info = idx_info;
+    return informacion;
+}
 
-    int d = 1;
-    int acumulado=0;
-    int p_aux = p-(p-1);
-    while(p!= 0)
-    {
-        for(i = p; i <= cantidad; i++)
-        {
-            if((i & (i-1))== 0)
+void generarArchivoDEX(int *cadena_H, int modulo, int largo_total, char *nombre_salida) {
+    int k, pos_en_bloque, bit_count;
+    unsigned char caracter = 0;
+
+   FILE *out = fopen(nombre_salida, "wb");
+    if (out == NULL) return;
+
+    bit_count = 0;
+    for (k = 1; k < largo_total; k++) {
+        pos_en_bloque = ((k - 1) % modulo) + 1; //este calculo sirve para calcular la pos relativa del bit dentro de su bloque de hamming actual
+        if ((pos_en_bloque & (pos_en_bloque - 1)) != 0) //filtramos bits de info
             {
-                printf("nada\n");
-            }
-            else
+            if (cadena_H[k] == 1) caracter |= (1 << (7 - bit_count)); //Bitmasking ,Si el bit es un 1, lo "prendemos" en la posición correspondiente.
+            bit_count++; // vamos contando para el bloque
+            if (bit_count == 8) //tenemos un byte
             {
-                acumulado = acumulado + (p_aux & i);
+                fputc(caracter, out); //escribimos el caracter en el archivoooo
+                caracter = 0;
+                bit_count = 0;
             }
         }
-        p_aux++;
-        p--;
     }
-    return 0;
-}*/
+    fclose(out);
+    printf("\nArchivo %s generado exitosamente.\n", nombre_salida);
+}
 
 
-int main()
-{
-    int *chain = malloc(2 *sizeof(int));
-    chain[0] = 0;
-    int i,n,t;
-    i = 1;
-    n = 1;
+int main() {
+    int *chain = malloc(1 * sizeof(int));
+    int i, n = 1, t, caracter, largo_h, largo_info;
+    int *cadena_H, *info_recuperada;
     FILE *arch;
-    arch = fopen("ej.txt","rb");
-    if(arch == 'NULL')
-    {
-        printf("Ocurrio un error");
-        return 0;
+
+    arch = fopen("ej.txt", "rb");
+    if (arch == NULL) {
+        printf("Error: No se encontro ej.txt\n");
+        return 1;
     }
 
-    int caracter;
-    while((caracter = fgetc(arch))!= EOF)
-    {
-        printf("%c", caracter);
-        printf("no salgp\n");
+    printf("Contenido del archivo (bits):\n");
+    while ((caracter = fgetc(arch)) != EOF) {
         imprimirB((unsigned char)caracter);
-        for(t= 7; t>=0; t--)
-        {
-            n+=1;
-            chain = realloc(chain, n * sizeof(int));
-            chain[i]= (((unsigned char)caracter >> t) & 1);
-            i++;
+        for (t = 7; t >= 0; t--) {
+            chain = realloc(chain, (n + 1) * sizeof(int));
+            chain[n] = ((unsigned char)caracter >> t) & 1;
+            n++;
         }
-
     }
     fclose(arch);
 
-    int salir = 0;
 
-   /* i =0;
-    printf("MOSTRAME ESTO %d\n",n);
-    while(i<n)
-    {
-      printf("%d", chain[i]);
-      i++;
-    }
-    printf("\n");
-    */
-    int *cadena_H = Hamming(chain,8,n);
-    i =0;
-    while(i<17)
-    {
+    cadena_H = Hamming(chain, 8, n, &largo_h);
 
-        printf("%d", cadena_H[i]);
-        if(i == 8)
-        {
-            printf("|||");
-        }
-        i++;
-    }
-    printf("\n AVER Q ONDA");
-    while(salir == 0){
+    printf("\nTrama Protegida: ");
+    for(i = 1; i < largo_h; i++) printf("%d", cadena_H[i]);
+    introducir_errores(cadena_H, 8, largo_h);
+    generarArchivoDEX(cadena_H, 8, largo_h, "archivo.DEX");
+  info_recuperada = decodificarHamming(cadena_H, 8, largo_h, &largo_info);
 
+    printf("\nInformacion Recuperada: ");
+    for(i = 0; i < largo_info; i++) printf("%d", info_recuperada[i]);
 
-        //printf("%d \n",(int)log2(32));
+    free(chain);
+    free(cadena_H);
+    free(info_recuperada);
 
-        printf("Ingrese una opcion\n");
-        printf("[1] Leer archivo txt\n");
-            salir =1;
-    }
     return 0;
 }
